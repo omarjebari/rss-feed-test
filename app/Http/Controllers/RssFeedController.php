f4cb0a99aeba681a13a32cc0f410696941a31907
omar jebari <994497+omarjebari@users.noreply.github.com>
2022-11-10 15:35:43 +0000

    Added routes and actions

---
+++ app/Http/Controllers/RssFeedController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RssFeedController extends Controller
+{
+    /**
+     * Display a list of feeds stored locally
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * import a feed
+     */
+    public function import()
+    {
+
+    }
+}
